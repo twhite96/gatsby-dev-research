9b20d41a0065993ce7893e96c1192a6d2e7d5f90
Tiffany <tiffany@tiffanywhite.dev>
2023-03-24 19:47:46 -0400

    static

--- _layouts/gatsby-ssr.js
+++ _layouts/gatsby-ssr.js
@@ -1,22 +1,33 @@
-// import React from "react";
+import React from "react";
 
-// export const onRenderBody = ({ setPostBodyComponents }) => {
-//   setPostBodyComponents([
-//     <>
-//       <script
-//         src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/prism.min.js'
-//         integrity='sha512-axJX7DJduStuBB8ePC8ryGzacZPr3rdLaIDZitiEgWWk2gsXxEFlm4UW0iNzj2h3wp5mOylgHAzBzM4nRSvTZA=='
-//         crossorigin='anonymous'
-//         referrerpolicy='no-referrer'
-//       ></script>
-//       <link
-//         rel='stylesheet'
-//         href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css'
-//         integrity='sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ=='
-//         crossorigin='anonymous'
-//         referrerpolicy='no-referrer'
-//       />
-//       ,
-//     </>,
-//   ]);
-// };
+const headComponents = [
+  <>
+    <script
+      src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/prism.min.js'
+      integrity='sha512-axJX7DJduStuBB8ePC8ryGzacZPr3rdLaIDZitiEgWWk2gsXxEFlm4UW0iNzj2h3wp5mOylgHAzBzM4nRSvTZA=='
+      crossorigin='anonymous'
+      referrerpolicy='no-referrer'
+    ></script>
+    ,
+    <link
+      rel='stylesheet'
+      href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css'
+      integrity='sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ=='
+      crossorigin='anonymous'
+      referrerpolicy='no-referrer'
+    />
+    <link rel='stylesheet' href='css/style.css' key='custom' />,
+    <link rel='stylesheet' href='css/laserwave.css' key='custom' />,
+    <link
+      rel='shortcut icon'
+      href='images/favicon.svg'
+      type='image/svg+xml'
+      key='favicon'
+    />
+    ,
+  </>,
+];
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents(headComponents);
+};
