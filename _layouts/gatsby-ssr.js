1b2779c8ce4f09ff3ea61969e2d96a7c86fa5518
Tiffany <tiffany@tiffanywhite.dev>
2023-03-24 05:39:27 -0400

    Prism

--- _layouts/gatsby-ssr.js
+++ _layouts/gatsby-ssr.js
@@ -4,29 +4,49 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
     <>
       <script
-        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js'
-        integrity='sha512-7Z9J3l1+EYfeaPKcGXu3MS/7T+w19WtKQY/n+xzmw4hZhJ9tyYmcUS+4QqAlzhicE5LAfMQSF3iFTK9bQdTxXg=='
+        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/prism.min.js'
+        integrity='sha512-axJX7DJduStuBB8ePC8ryGzacZPr3rdLaIDZitiEgWWk2gsXxEFlm4UW0iNzj2h3wp5mOylgHAzBzM4nRSvTZA=='
         crossorigin='anonymous'
         referrerpolicy='no-referrer'
       ></script>
       <script
-        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-jsx.min.js'
-        integrity='sha512-m3JYEI6gx5fh9jF10FjGoMzVKcV2N6nchcDcqPCdI1L3R2WQV7br2XVNR8iTLb2daOMRl3zldbcfT40xU2ntVw=='
+        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/components/prism-jsx.min.js'
+        integrity='sha512-KNvdEaPrmTihMKmH/8CeHfMskGdt5WaPyMoNWVkg39w9/itQA68/tNPi2zcczyr2RvRprXai8aH8zftj89Jg1g=='
         crossorigin='anonymous'
         referrerpolicy='no-referrer'
       ></script>
       <script
-        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-python.min.js'
-        integrity='sha512-AKaNmg8COK0zEbjTdMHJAPJ0z6VeNqvRvH4/d5M4sHJbQQUToMBtodq4HaV4fa+WV2UTfoperElm66c9/8cKmQ=='
+        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/components/prism-python.min.js'
+        integrity='sha512-nvWJ2DdGeQzxIYP5eo2mqC+kXLYlH4QZ/AWYZ/yDc5EqM74jiC5lxJ+8d+6zI/H9MlsIIjrJsaRTgLtvo+Jy6A=='
         crossorigin='anonymous'
         referrerpolicy='no-referrer'
       ></script>
       <script
-        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-tsx.min.js'
-        integrity='sha512-xjGCJ9YxyZBfYTCHsEjkOZMoOse1W3cKMXv1szXrxs68myuXt0YTj3/xKPar6iDMlXzTUSEqwUxprWcyp+plaw=='
+        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/components/prism-tsx.min.js'
+        integrity='sha512-ueCte/ZwNI460xA3ipwvMk/emlgFYNZGBLlc3EMqZqJbQoLaOhoCpY1l6dHbfNz2p+A9E/sQoUdNRVvu/cqQEw=='
         crossorigin='anonymous'
         referrerpolicy='no-referrer'
       ></script>
+      <script
+        src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/components/prism-ruby.min.js'
+        integrity='sha512-nw4lRvuD47DTW65G2USfNSu0VSPdl+vJSMz5sR+YQ3/HtG7w4S7pDvh1ygqVuOVSpHDHKqteZ2GroJAPke+3JQ=='
+        crossorigin='anonymous'
+        referrerpolicy='no-referrer'
+      ></script>
+      <link
+        rel='stylesheet'
+        href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css'
+        integrity='sha512-tN7Ec6zAFaVSG3TpNAKtk4DOHNpSwKHxxrsiw4GHKESGPs5njn/0sMCUMl2svV4wo4BK/rCP7juYz+zx+l6oeQ=='
+        crossorigin='anonymous'
+        referrerpolicy='no-referrer'
+      />
+      <link
+        rel='stylesheet'
+        href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/plugins/line-highlight/prism-line-highlight.min.css'
+        integrity='sha512-nXlJLUeqPMp1Q3+Bd8Qds8tXeRVQscMscwysJm821C++9w6WtsFbJjPenZ8cQVMXyqSAismveQJc0C1splFDCA=='
+        crossorigin='anonymous'
+        referrerpolicy='no-referrer'
+      />
     </>,
   ]);
 };
