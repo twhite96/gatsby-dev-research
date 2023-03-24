9b0a03f7ab55c4781fea690c3a9013a8b8ccdbc2
Tiffany <tiffany@tiffanywhite.dev>
2023-03-24 19:19:56 -0400

    Fix file path

--- _layouts/gatsby-browser.js
+++ _layouts/gatsby-browser.js
@@ -1,5 +1,4 @@
-require("prismjs/themes/coy.css")
-
+require("node_modules/prismjs/themes/prism-coy.css")
 import "@fontsource/source-sans-pro" // Defaults to weight 400 with all styles included.
 import "@fontsource/bree-serif" // Defaults to weight 400 with all styles included.
 import "@fontsource/fira-code" // Defaults to weight 400 with all styles included.
