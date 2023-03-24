390ce002ac5a76e36d86668138800ad29aa24e96
Tiffany <tiffany@tiffanywhite.dev>
2023-03-24 19:33:18 -0400

    Cat

--- _layouts/gatsby-browser.js
+++ _layouts/gatsby-browser.js
@@ -1,6 +1,6 @@
-require("node_modules/prismjs/themes/prism-coy.css")
 import "@fontsource/source-sans-pro" // Defaults to weight 400 with all styles included.
 import "@fontsource/bree-serif" // Defaults to weight 400 with all styles included.
 import "@fontsource/fira-code" // Defaults to weight 400 with all styles included.
 import "@fontsource/inter" // Defaults to weight 400 with all styles included.
 import "./assets/styles.css"
+import './assets/laserwave.css'
