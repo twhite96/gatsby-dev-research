3847432a1a43a350403a4feebbcd9b73477a4714
Tiffany <tiffany@tiffanywhite.dev>
2023-03-24 19:01:10 -0400

    Feat gatsby prism plugin

--- _layouts/gatsby-browser.js
+++ _layouts/gatsby-browser.js
@@ -1,3 +1,5 @@
+require("prismjs/themes/prism-laserwave-italic.css")
+
 import "@fontsource/source-sans-pro" // Defaults to weight 400 with all styles included.
 import "@fontsource/bree-serif" // Defaults to weight 400 with all styles included.
 import "@fontsource/fira-code" // Defaults to weight 400 with all styles included.
